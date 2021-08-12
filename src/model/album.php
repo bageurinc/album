265e74c67bf8f568802f0cb1902a9d4f5001e57e
Adnan Firmansyah <adnan.bayufirmansyah@gmail.com>
2021-08-12 17:42:33 +0700

    tambah batch

--- src/model/album.php
+++ src/model/album.php
@@ -2,6 +2,7 @@
 
 namespace Bageur\Album\Model;
 
+use App\training;
 use Illuminate\Database\Eloquent\Model;
 use Bageur\Album\processors\AvatarProcessor;
 use Bageur\Album\model\albumdetail;
@@ -10,7 +11,23 @@ use Bageur\Artikel\Model\komentar;
 class album extends Model
 {
     protected $table = 'bgr_album';
-    protected $appends = ['avatar','avatar_group'];
+    protected $appends = ['avatar','avatar_group', 'batch'];
+
+
+    public function getBatchAttribute()
+    {
+        // $split = explode("|", $this->nama);
+        // $training_batch = training::where('kelompok', $split[0])->first();
+        // if ($training_batch) {
+        //     return (int)$training_batch->batch;
+        // }
+        // else {
+        //     return 0;
+        // }
+        $split = explode(" | ",$this->nama);
+        $split_batch = explode(" ",$split[0]);
+        return (int) end($split_batch);
+    }
 
     public function getAvatarAttribute()
     {
@@ -19,7 +36,7 @@ class album extends Model
                                 ->first();
 
             return AvatarProcessor::get($this->nama,@$last->gambar);
-    }       
+    }
     public function getAvatarGroupAttribute()
     {
             $last = albumdetail::orderBy('created_at','desc')
@@ -27,11 +44,11 @@ class album extends Model
                                 ->first();
 
             return AvatarProcessor::get($this->group,@$last->gambar);
-    }  
+    }
     public function komentar()
     {
         return $this->hasMany(komentar::class, 'album_id', 'id');
-    } 
+    }
     public function scopeDatatable($query,$request,$page=12)
     {
         $search       = ["nama",'`group`'];
@@ -44,7 +61,7 @@ class album extends Model
             }else{
                 $searchqry .= "OR lower($value) like '%".strtolower($request->search)."%'";
             }
-        } 
+        }
 
         $searchqry .= ")";
         if(@$request->sort_by){
